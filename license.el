9ea8c84ccb74c43dc99f632873473e419e4bcd1c
condy <condy0919@gmail.com>
2020-07-07 01:23:12 +0800

    Reformat: NO TABS

--- license.el
+++ license.el
@@ -501,10 +501,10 @@ nil means not to use project information."
                 ('projectile (projectile-project-name))
                 ;; FIXME: any better way?
                 ('project (directory-file-name
-			   (let ((proj (project-current)))
-			     (if (stringp proj)
-				 proj
-			       (cdr proj)))))
+                           (let ((proj (project-current)))
+                             (if (stringp proj)
+                                 proj
+                               (cdr proj)))))
                 (_ nil))))
     (when name
       (concat name " Authors"))))
