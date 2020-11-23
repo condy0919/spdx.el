9beb8b03070bcda53068c2c9538a178ed0d5a6ee
Lassi Kortela <lassi@lassi.io>
2020-11-23 23:51:55 +0200

    Fix package-lint warnings

--- license.el
+++ license.el
@@ -5,6 +5,8 @@
 ;; Author: Zhiwei Chen <condy0919@gmail.com>
 ;; Keywords: license, tools
 ;; URL: https://github.com/condy0919/license.el
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
