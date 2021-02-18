d7ca7bd9d0cb721bc21ef86d1db4fc30a14a5ba1
Lassi Kortela <lassi@lassi.io>
2021-02-18 15:19:13 +0200

    Add script to update SPDX data from the internet
    Just run admin/spdx-update.sh

---
+++ admin/spdx-update.el
@@ -0,0 +1,47 @@
+(require 'cl-lib)
+
+(defun spdx-update--get-license-ids ()
+  (with-temp-buffer
+    (url-insert-file-contents
+     (concat "https://raw.githubusercontent.com/"
+             "spdx/license-list-data"
+             "/master/"
+             "json/licenses.json"))
+    (let* ((json (json-read))
+           (release-date (cdr (assoc 'releaseDate json)))
+           (licenses (cdr (assoc 'licenses json)))
+           (license-ids (mapcar (lambda (license)
+                                  (cdr (assoc 'licenseId license)))
+                                licenses)))
+      (list release-date license-ids))))
+
+(defun spdx-update ()
+  (cl-destructuring-bind (release-date license-ids)
+      (spdx-update--get-license-ids)
+    (with-temp-buffer
+      (insert
+       ";;; spdx-data.el --- SPDX license database"
+       " -*- lexical-binding: t -*-" "\n"
+       ";;" "\n"
+       ";; Copyright 2020 SPDX" "\n"
+       ";; SPDX-License-Identifier: MIT" "\n"
+       ";;" "\n"
+       ";;; Commentary:" "\n"
+       ";;" "\n"
+       ";; Automatically converted from licenses.json." "\n"
+       ";;" "\n"
+       ";;; Code:" "\n"
+       "\n"
+       (format "(defconst spdx-data-release-date %S)\n" release-date)
+       "\n"
+       "(defconst spdx-data-license-identifiers" "\n"
+       "  '(\n")
+      (dolist (license-id (butlast license-ids))
+        (insert (format "    %S\n" license-id)))
+      (let ((license-id (car (last license-ids))))
+        (insert (format "    %S))\n" license-id)))
+      (insert "\n"
+              "(provide 'spdx-data)" "\n"
+              "\n"
+              ";;; spdx-data.el ends here" "\n")
+      (write-region (point-min) (point-max) "spdx-data.el.new"))))
---
+++ admin/spdx-update.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -eu
+cd "$(dirname "$0")"
+echo "Entering directory '$PWD'"
+set -x
+${EMACS:-emacs} -Q -batch -L . -l spdx-update -f spdx-update
+mv -f spdx-data.el.new ../spdx-data.el
