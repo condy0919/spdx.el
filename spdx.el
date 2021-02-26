7b8e3da624e91f955f3f06daf3b37534d386964a
Lassi Kortela <lassi@lassi.io>
2021-02-26 17:32:24 +0200

    Upgrade Emacs dependency from 24.1 to 24.4
    To allow (require 'subr-x) which get string-trim etc.

--- spdx.el
+++ spdx.el
@@ -6,7 +6,7 @@
 ;; Keywords: license, tools
 ;; URL: https://github.com/condy0919/spdx.el
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "24.1"))
+;; Package-Requires: ((emacs "24.4"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
