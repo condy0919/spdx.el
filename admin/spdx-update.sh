b6b09128069383feec1afeabb60956c9d507ed68
Lassi Kortela <lassi@lassi.io>
2021-02-28 11:46:40 +0200

    Make admin/spdx-update.sh do a git commit

--- admin/spdx-update.sh
+++ admin/spdx-update.sh
@@ -5,3 +5,5 @@ echo "Entering directory '$PWD'"
 set -x
 ${EMACS:-emacs} -Q -batch -L . -l spdx-update -f spdx-update
 mv -f spdx-data.el.new ../spdx-data.el
+git commit --message "Update SPDX data" ../spdx-data.el
+git --no-pager show
